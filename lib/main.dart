364f596b5f87cb5f008045b168e9629097ba37cc
pardeep2690 <bigbenprojects@gmail.com>
2022-12-16 21:08:32 +0530

    some modofications

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 
@@ -6,232 +6,209 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _total = 0;
+  final _2000Controller = TextEditingController();
+  final _500Controller = TextEditingController();
+  final _200Controller = TextEditingController();
+  final _100Controller = TextEditingController();
+  final _50Controller = TextEditingController();
+  final _20Controller = TextEditingController();
+  final _10Controller = TextEditingController();
+
+  void _refresh() {
+    setState(() {
+      _total = 0;
+      _2000Controller.clear();
+      _500Controller.clear();
+      _200Controller.clear();
+      _100Controller.clear();
+      _50Controller.clear();
+      _20Controller.clear();
+      _10Controller.clear();
+    });
+  }
+
+  void _updateTotal() {
+    setState(() {
+      _total = 0;
+      if (_2000Controller.text.isNotEmpty) {
+        _total += 2000 * int.parse(_2000Controller.text);
+      }
+      if (_500Controller.text.isNotEmpty) {
+        _total += 500 * int.parse(_500Controller.text);
+      }
+      if (_200Controller.text.isNotEmpty) {
+        _total += 200 * int.parse(_200Controller.text);
+      }
+      if (_100Controller.text.isNotEmpty) {
+        _total += 100 * int.parse(_100Controller.text);
+      }
+      if (_50Controller.text.isNotEmpty) {
+        _total += 50 * int.parse(_50Controller.text);
+      }
+      if (_20Controller.text.isNotEmpty) {
+        _total += 20 * int.parse(_20Controller.text);
+      }
+      if (_10Controller.text.isNotEmpty) {
+        _total += 10 * int.parse(_10Controller.text);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cashier Calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: CashierCalculator(),
-    );
-  }
-}
-
-class CashierCalculator extends StatefulWidget {
-  @override
-  _CashierCalculatorState createState() => _CashierCalculatorState();
-}
-
-class _CashierCalculatorState extends State<CashierCalculator> {
-  // ignore: non_constant_identifier_names
-  int _2000notes = 0;
-  int _500notes = 0;
-  int _200notes = 0;
-  int _100notes = 0;
-  int _50notes = 0;
-  int _20notes = 0;
-  int _10notes = 0;
-  int _5notes = 0;
-  int _2notes = 0;
-  int _1notes = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('Cashier Calculator'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Text('Enter the number of notes'),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _2000notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '2000'),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _500notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '500'),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _200notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '200'),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _100notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '100'),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _50notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '50'),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _20notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '20'),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _10notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '10'),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _5notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '5'),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _2notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '2'),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _1notes = int.parse(value);
-                });
-              },
-              decoration: const InputDecoration(labelText: '1'),
-            ),
-            const Text('Total Amount'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('2000 : '),
-                Text('${_2000notes * 2000}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('500 : '),
-                Text('${_500notes * 500}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('200 : '),
-                Text('${_200notes * 200}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('100 : '),
-                Text('${_100notes * 100}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('50 : '),
-                Text('${_50notes * 50}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('20 : '),
-                Text('${_20notes * 20}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('10 : '),
-                Text('${_10notes * 10}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('5 : '),
-                Text('${_5notes * 5}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('2 : '),
-                Text('${_2notes * 2}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('1 : '),
-                Text('${_1notes * 1}'),
-              ],
-            ),
-            const Text('Total Amount : '),
-            Text(
-                '${_2000notes * 2000 + _500notes * 500 + _200notes * 200 + _100notes * 100 + _50notes * 50 + _20notes * 20 + _10notes * 10 + _5notes * 5 + _2notes * 2 + _1notes * 1}'),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bank Note Total'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _2000notes = 0;
-            _500notes = 0;
-            _200notes = 0;
-            _100notes = 0;
-            _50notes = 0;
-            _20notes = 0;
-            _10notes = 0;
-            _5notes = 0;
-            _2notes = 0;
-            _1notes = 0;
-          });
-        },
-        child: const Icon(Icons.refresh),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('2000 x '),
+                    Expanded(
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        textInputAction: TextInputAction.next,
+                        controller: _2000Controller,
+                        onChanged: (value) => _updateTotal(),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Text(
+                        '= ${_2000Controller.text.isNotEmpty ? 2000 * int.parse(_2000Controller.text) : 0}'),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('500 x '),
+                    Expanded(
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        textInputAction: TextInputAction.next,
+                        controller: _500Controller,
+                        onChanged: (value) => _updateTotal(),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Text(
+                        '= ${_500Controller.text.isNotEmpty ? 500 * int.parse(_500Controller.text) : 0}'),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('200 x '),
+                    Expanded(
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        textInputAction: TextInputAction.next,
+                        controller: _200Controller,
+                        onChanged: (value) => _updateTotal(),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Text(
+                        '= ${_200Controller.text.isNotEmpty ? 200 * int.parse(_200Controller.text) : 0}'),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('100 x '),
+                    Expanded(
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        textInputAction: TextInputAction.next,
+                        controller: _100Controller,
+                        onChanged: (value) => _updateTotal(),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Text(
+                        '= ${_100Controller.text.isNotEmpty ? 100 * int.parse(_100Controller.text) : 0}'),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('50 x '),
+                    Expanded(
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        textInputAction: TextInputAction.next,
+                        controller: _50Controller,
+                        onChanged: (value) => _updateTotal(),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Text(
+                        '= ${_50Controller.text.isNotEmpty ? 50 * int.parse(_50Controller.text) : 0}'),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('20 x '),
+                    Expanded(
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        textInputAction: TextInputAction.next,
+                        controller: _20Controller,
+                        onChanged: (value) => _updateTotal(),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Text(
+                        '= ${_20Controller.text.isNotEmpty ? 20 * int.parse(_20Controller.text) : 0}'),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('10 x '),
+                    Expanded(
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        textInputAction: TextInputAction.done,
+                        controller: _10Controller,
+                        onChanged: (value) => _updateTotal(),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Text(
+                        '= ${_10Controller.text.isNotEmpty ? 10 * int.parse(_10Controller.text) : 0}'),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text('Total: $_total'),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _refresh,
+          child: const Icon(Icons.refresh),
+        ),
       ),
     );
   }
